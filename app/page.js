56515e71cf9ca65f970b9f7b7b27d03e8de5db4a
Reshmi Ramesh <reshmiramesh07@gmail.com>
2024-08-09 08:44:07 -0700

    set up add item ui

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-// 15:23
+// 30:17
 
 'use client'
 import Image from "next/image";
 import {useState, useEffect} from "react"
 import {firestore} from "@/firebase"
-import {Box, Typography} from "@mui/material"
+import {Box, Button, Modal, Stack, TextField, Typography} from "@mui/material"
 import {query, collection, getDocs, doc, deleteDoc} from "firebase/firestore"
 
 export default function Home() {
@@ -66,7 +66,59 @@ export default function Home() {
   const handleClose = () => setOpen(false)
 
   return (
-    <Box>
+    <Box 
+      width="100vw" 
+      height="100vh" 
+      display="flex" 
+      justifyContent="center" 
+      alignItems="center"
+      gap={2}
+    >
+      <Modal 
+        open={open}
+        onClose={handleClose}
+      >
+        <Box
+          position="absolute"
+          top="50%"
+          left="50%"
+          transform="translate(-50%, -50%)"
+          width={400}
+          bgcolor="white"
+          border="2px solid #000"
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+          sx={{
+            transform: "translate(-50%, -50%)"
+          }}
+        >
+          <Typography variant="h6">Add Item</Typography>
+          <Stack width="100%" direction="row" spacing={2}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              value={itemName}
+              onChange={(e) => {
+                setItemName(e.target.value)
+              }}
+            />
+            <Button 
+              variant="contained"
+              onClick={() => {
+                addItem(itemName)
+                setItemName('')
+                handleClose()
+              }}
+            >
+              Add
+            </Button>
+          </Stack>
+        </Box>
+        
+      </Modal>
       <Typography variant="h1">Inventory Management</Typography>
     </Box>
   )
